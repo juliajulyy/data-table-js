2840705330734b56b9fcf48d102620a576488267
Yulia Cheliak <juliacheliak@gmail.com>
2020-07-28 15:26:59 +0300

    fix validation

--- js/add-worker.js
+++ js/add-worker.js
@@ -2,8 +2,10 @@ const addWorker = () => {
   const workers = getWorker();
   const newWorker = createWorker();
 
-  (validateName(newWorker.firstName) && validateName(newWorker.lastName)) ? setWorker([
+  if (validateName(newWorker.firstName) && validateName(newWorker.lastName)) {
+    setWorker([
       ...(workers === null) ? [] : workers,
       newWorker
-  ]) : alert('Please fill in all fields with more than two characters.');
+    ])
+  }
 }
--- js/worker-factory.js
+++ js/worker-factory.js
@@ -1,8 +1,8 @@
 class NewWorker {
   constructor(firstName, lastName, position) {
     this.id = (getWorker() === null) ? 1 : getWorker().length + 1;
-    this.firstName = firstName;
-    this.lastName = lastName;
+    this.firstName = firstName[0].toUpperCase() +  firstName.toLowerCase().substring(1);
+    this.lastName = lastName[0].toUpperCase() + lastName.toLowerCase().substring(1);
     this.position = position;
     this.createdDate = (new Date().getMonth() + 1) + '/' + new Date().getDate() + '/' + new Date().getFullYear();
   }
