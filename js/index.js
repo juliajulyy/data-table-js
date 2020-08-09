ed06b3343673e47286a261136e25a379fdea4b53
Yulia Cheliak <juliacheliak@gmail.com>
2020-08-09 15:44:30 +0300

    sel declare workers

--- js/index.js
+++ js/index.js
@@ -1,35 +1,54 @@
 const main = () => {
 
-  // const workers = [
-  //   {
-  //     id: 1,
-  //     firstName: "John",
-  //     lastName: "Pool",
-  //     position: "Front-end dev",
-  //     createdDate: "11/12/2019"
-  //   },
-  //   {
-  //     id: 2,
-  //     firstName: "Mia",
-  //     lastName: "Chester",
-  //     position: "Front-end dev",
-  //     createdDate: "02/03/2020"
-  //   },
-  //   {
-  //     id: 3,
-  //     firstName: "James",
-  //     lastName: "Frazier",
-  //     position: "HR",
-  //     createdDate: "21/06/2020"
-  //   }
-  // ];
+  const workers = [
+    {
+      id: 1,
+      firstName: "John",
+      lastName: "Pool",
+      position: "Front-end dev",
+      createdDate: "11/12/2019"
+    },
+    {
+      id: 2,
+      firstName: "Mia",
+      lastName: "Chester",
+      position: "Front-end dev",
+      createdDate: "02/03/2020"
+    },
+    {
+      id: 3,
+      firstName: "James",
+      lastName: "Frazier",
+      position: "HR",
+      createdDate: "21/06/2020"
+    },
+    {
+      id: 4,
+      firstName: "Matt",
+      lastName: "Smith",
+      position: "Back-end dev",
+      createdDate: "11/07/2020"
+    },
+    {
+      id: 5,
+      firstName: "Joe",
+      lastName: "Cole",
+      position: "Tester",
+      createdDate: "01/08/2020"
+    },
+    {
+      id: 6,
+      firstName: "Ulyana",
+      lastName: "Kravchenko",
+      position: "Front-end dev",
+      createdDate: "03/07/2020"
+    }
+  ];
 
-  // const workers = getWorker();
+  if (!localStorage["workers"]) {
+    workers.forEach(item => setWorker(item))
+  }
 
-  // setWorker([
-  //   ...(workers === null) ? [] : workers,
-  //   ...workers,
-  // ]);
   const addNewbtn = document.querySelector("#add-new");
   addNewbtn.addEventListener('click', addWorker);
 
